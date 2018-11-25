34d67300bc4f1c194f8ad73c0d8ebabeee382040
abdulrahman_mac <thobahneework>
2018-11-25 19:37:20 +0300

    First commit try

---
+++ app/src/main/java/co/infinitecloud/gittest/MainActivity.java
@@ -0,0 +1,16 @@
+package co.infinitecloud.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"OK",Toast.LENGTH_SHORT).show();
+    }
+}
